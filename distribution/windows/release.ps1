86e22380d8f8e648588a15e3368416834d40c1b9
Evandro Pomatti <evandro.pomatti@gmail.com>
2021-08-17 18:46:57 -0300

    release

--- distribution/windows/release.ps1
+++ distribution/windows/release.ps1
@@ -1,6 +1,17 @@
-mkdir "C:\dist"
-$dir="C:\dist"
-mkdir $dir
-cd $dir
-Invoke-WebRequest https://nodejs.org/dist/v14.17.5/node-v14.17.5-win-x86.zip -OutFile node.zip
-Expand-Archive node.zip
+$dir = New-Item -Path "c:\" -Name "dist" -ItemType "directory"
+
+Invoke-WebRequest "https://nodejs.org/dist/v14.17.5/node-v14.17.5-win-x86.zip" -OutFile "$dir\node.zip"
+Expand-Archive "$dir\node.zip" -DestinationPath $dir
+Rename-Item "$dir\node-v14.17.5-win-x86" "$dir\node"
+Remove-Item "$dir\node.zip"
+
+Copy-Item -Path "src" -Destination "$dir\node" -Recurse
+Copy-Item -Path "package*.json" -Destination "$dir\node"
+Copy-Item -Path ".\distribution\windows\app.cmd" -Destination $dir
+Copy-Item -Path ".\distribution\windows\dist.env" -Destination "$dir\node\.env"
+
+cd $dir\node
+.\npm install
+
+$output = New-Item -Path "c:\" -Name "output" -ItemType "directory"
+Compress-Archive -Path $dir -DestinationPath "$output\excel2csv.zip"
